f5eda530d597af08d7f6aac74fa3334feec3785e
dinnksis <dina.kepecheva@gmail.com>
2024-12-09 22:00:13 +0300

    Create main.py

---
+++ bot/main.py
@@ -0,0 +1,35 @@
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import API_TOKEN
+from moneybot.registration import register_handlers
+from moneybot.handlers import registerr_handlers
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.state import StateFilter
+from database import init_db
+
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+dp.message.filter(StateFilter)
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+init_db()
+
+register_handlers(dp)
+registerr_handlers(dp)
+
+
+async def main():
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
