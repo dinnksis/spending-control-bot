98805282a1d48f458fae13537f7d435617fbeb84
dinnksis <dina.kepecheva@gmail.com>
2024-12-10 10:25:19 +0300

    Update database_safety.py

--- bot/database_safety.py
+++ bot/database_safety.py
@@ -21,7 +21,39 @@ def is_user_registered(login):
         with closing(conn.cursor()) as cur:
             cur.execute('SELECT login FROM safety WHERE login = ?', (login,))
             return cur.fetchone() is not None
+import sqlite3
+from contextlib import closing
+import bcrypt
+# для хэширования паролей
 
+
+# хэшированный пароль
+def add_user_to_database(login: str, password: str) -> None:
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    with sqlite3.connect('../moneybot/expenses.db') as conn:
+        with closing(conn.cursor()) as cur:
+            cur.execute(
+                'INSERT INTO users (login, password) VALUES (?, ?)',
+                (login, hashed_pw,)
+            )
+            conn.commit()
+
+
+# Функция проверки, зарегистрирован ли пользователь
+def is_user_registered(login: str) -> bool:
+    with sqlite3.connect('../moneybot/expenses.db') as conn:
+        with closing(conn.cursor()) as cur:
+            cur.execute('SELECT login FROM users WHERE login = ?', (login.lower(),))
+            return cur.fetchone() is not None
+
+
+# Функция проверки пароля
+def check_password(login: str, password: str) -> bool:
+    with sqlite3.connect('../moneybot/expenses.db') as conn:
+        with closing(conn.cursor()) as cur:
+            cur.execute('SELECT password FROM users WHERE login = ?', (login.lower(),))
+            result = cur.fetchone()
+            return result and bcrypt.checkpw(password.encode('utf-8'), result[0])
 
 # Функция проверки пароля
 def check_password(login,password):
