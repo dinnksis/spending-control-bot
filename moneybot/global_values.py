582a5e6b134e4cde0786adbfe6beaa261b797f48
kristmakevel <kristmakevel@gmail.com>
2024-12-12 21:03:54 +0300

    new global values (dicts) + func

--- moneybot/global_values.py
+++ moneybot/global_values.py
@@ -1,2 +1,13 @@
-login = ''
-is_authorized = False
+login = {}
+is_authorized = {}
+
+
+def checking_auth(us_id):
+    if us_id not in is_authorized:
+        is_authorized[us_id] = False
+        login[us_id] = ''
+        return False
+    else:
+        if is_authorized[us_id]:
+            return True
+        return False
